7370b81045bf43bda7c5d21b21a51a57497f5dde
Liz Hennessy <lizhennessy@Lizs-MacBook-Pro.local>
2024-09-24 14:00:53 -0600

    add pubsub, mutation resolver

--- messages/src/__generated__/resolvers-types.ts
+++ messages/src/__generated__/resolvers-types.ts
@@ -68,16 +68,6 @@ export type QueryConversationArgs = {
   recipientId: Scalars['ID']['input'];
 };
 
-export type Subscription = {
-  __typename?: 'Subscription';
-  listenForMessageInConversation?: Maybe<Message>;
-};
-
-
-export type SubscriptionListenForMessageInConversationArgs = {
-  id: Scalars['ID']['input'];
-};
-
 export type User = {
   __typename?: 'User';
   id: Scalars['ID']['output'];
@@ -173,7 +163,6 @@ export type ResolversTypes = ResolversObject<{
   Mutation: ResolverTypeWrapper<{}>;
   NewMessageInput: NewMessageInput;
   Query: ResolverTypeWrapper<{}>;
-  Subscription: ResolverTypeWrapper<{}>;
   User: ResolverTypeWrapper<UserRepresentation>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
 }>;
@@ -187,7 +176,6 @@ export type ResolversParentTypes = ResolversObject<{
   Mutation: {};
   NewMessageInput: NewMessageInput;
   Query: {};
-  Subscription: {};
   User: UserRepresentation;
   Boolean: Scalars['Boolean']['output'];
 }>;
@@ -218,10 +206,6 @@ export type QueryResolvers<ContextType = DataSourceContext, ParentType extends R
   conversations?: Resolver<Maybe<Array<Maybe<ResolversTypes['Conversation']>>>, ParentType, ContextType>;
 }>;
 
-export type SubscriptionResolvers<ContextType = DataSourceContext, ParentType extends ResolversParentTypes['Subscription'] = ResolversParentTypes['Subscription']> = ResolversObject<{
-  listenForMessageInConversation?: SubscriptionResolver<Maybe<ResolversTypes['Message']>, "listenForMessageInConversation", ParentType, ContextType, RequireFields<SubscriptionListenForMessageInConversationArgs, 'id'>>;
-}>;
-
 export type UserResolvers<ContextType = DataSourceContext, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = ResolversObject<{
   __resolveReference?: ReferenceResolver<Maybe<ResolversTypes['User']>, { __typename: 'User' } & GraphQLRecursivePick<ParentType, {"id":true}>, ContextType>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
@@ -233,7 +217,6 @@ export type Resolvers<ContextType = DataSourceContext> = ResolversObject<{
   Message?: MessageResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
-  Subscription?: SubscriptionResolvers<ContextType>;
   User?: UserResolvers<ContextType>;
 }>;
 
